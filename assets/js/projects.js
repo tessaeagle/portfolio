432d7f7c410e2b27d755d0120c2b6e2f5cf41798
tessaeagle <eagle.tessa@gmail.com>
2020-12-22 14:52:47 -0800

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(() => {
     render_projects('featured');
 })
@@ -9,60 +11,150 @@ let render_projects = (slug) => {
     $('.white-button').removeClass('white-button-hover');
     $(`#${slug}`).addClass('white-button-hover');
 
-    let projects_obj = [ 
+    let projects_obj = [
         {
-            image: 'assets/images/apex.png',
-            link: 'https://www.brainstimjrnl.com/article/S1935-861X(18)30518-7/fulltext',
-            title: 'NANS 2018',
+            image: 'assets/images/mentors.jpg',
+            link: 'https://github.com/abhn/Mporter',
+            title: 'Mporter',
             demo: 'https://mporter.co',
             technologies: ['Flask', 'Celery', 'Python'],
-            description: "2018 Neuromodulation Society Poster",
-            categories: ['posters']
+            description: "Flask web application for easy reporting updates to one's mentor. Multi-user support, easy to deploy and use.",
+            categories: ['featured', 'webdev']
         },
         {
-            image: 'assets/images/APA_poster.png',
-            link: 'https://github.com/tessaeagle/tessaeagle.github.io/blob/master/assets/images/APA_Poster.png',
-            title: 'APA Interoception',
-            //demo: 'https://mporter.co',
-           //technologies: ['Flask', 'Celery', 'Python'],
-            description: "APA Poster on Interoception",
-            categories: ['posters']
+            image: 'assets/images/mobile-landscape.jpg',
+            link: 'https://github.com/abhn/Wall-E',
+            title: 'Wall-E',
+            demo: 'http://wall-e-jekyll.github.io/',
+            technologies: ['Semantic UI', 'Jekyll'],
+            description: "A modern Jekyll theme with grid frontpage, beautiful typography, mobile responsive, made with Semantic UI.",
+            categories: ['featured', 'webdev']
         },
         {
-            image: 'assets/images/BU_Poster.png',
-            link: 'https://github.com/tessaeagle/tessaeagle.github.io/blob/master/assets/images/BU_Poster.png',
-            title: 'BUCLD Poster',
-            //demo: 'https://mporter.co',
-           //technologies: ['Flask', 'Celery', 'Python'],
-            description: "BUCLD Poster on Undergrad Honors Thesis",
-            categories: ['posters']
+            image: 'assets/images/collage.jpg',
+            link: 'https://github.com/abhn/Marvel',
+            title: 'Marvel',
+            demo: false,
+            technologies: ['Android', 'OpenCV'],
+            description: "Attendance marking tool that uses face recognition for marking attendance and firebase for tracking and analytics.",
+            categories: ['featured', 'native']
         },
         {
-            image: 'assets/images/poster.png',
-            link: 'https://github.com/tessaeagle/tessaeagle.github.io/blob/master/assets/images/poster.png',
-            title: 'Undergraduate Honors Thesis',
-            //demo: 'https://mporter.co',
-           //technologies: ['Flask', 'Celery', 'Python'],
-            description: "Undergraduate Research Expo",
-            categories: ['posters']
-        },
-         {
-            image: 'assets/images/paper.png',
-            link: 'https://github.com/tessaeagle/tessaeagle.github.io/blob/master/assets/images/poster.png',
-            title: 'MS Treatment Satisfaction',
-            //demo: 'https://mporter.co',
-           //technologies: ['Flask', 'Celery', 'Python'],
-            description: "MSARD Journal Paper",
-            categories: ['papers', 'featured']
+            image: 'assets/images/mpw.jpg',
+            link: 'https://github.com/abhn/mpw',
+            title: 'Master Password',
+            demo: 'https://www.nagekar.com/mpw',
+            technologies: ['Semantic UI', 'CSS3'],
+            description: "Master Password is an ingenious password solution that makes your passwords truly impossible to lose.",
+            categories: ['featured', 'security']
         },
         {
-            image: 'assets/images/8_18.png',
-            link: 'https://github.com/tessaeagle/TidyTuesday',
-            title: 'R4DS Tidy Tuesday',
-            //demo: 'https://mporter.co',
-           //technologies: ['Flask', 'Celery', 'Python'],
-            description: "Data Visualization Experiments",
-            categories: ['r', 'featured']
+            image: 'assets/images/social-share-count.jpeg',
+            link: 'https://github.com/abhn/Social-Share-Counts',
+            title: 'Social Share Count',
+            demo: false,
+            technologies: ['Python'],
+            description: "Ever wondered how many times a URL has been shared on popular social networks?",
+            categories: ['native']
+        },
+        {
+            image: 'assets/images/data-destroyer.png',
+            link: 'https://github.com/abhn/data-destroyer-gui',
+            title: 'Data Destroyer',
+            demo: false,
+            technologies: ['C++', 'Qt'],
+            description: "Native GUI wrapper for GNU coreutils tool 'dd'",
+            categories: ['native']
+        },
+        {
+            image: 'assets/images/raspberry-pi-monitor.png',
+            link: 'https://github.com/abhn/RPi-Status-Monitor',
+            title: 'Raspberry Pi Monitor',
+            demo: false,
+            technologies: ['python', 'flask'],
+            description: "Web based status monitor/smart mirror, displays system stats, weather and more.",
+            categories: ['webdev', 'diy']
+        },
+        {
+            image: 'assets/images/s3scan.png',
+            link: 'https://github.com/abhn/S3Scan',
+            title: 'S3Scan',
+            demo: false,
+            technologies: ['python'],
+            description: "Automate crawling of a website and find publicly open S3 buckets for takeover.",
+            categories: ['native', 'security']
+        },
+        {
+            image: 'assets/images/elementary.png',
+            link: 'https://github.com/abhn/Elementary',
+            title: 'Elementary',
+            demo: 'https://elementary-jekyll.github.io/',
+            technologies: ['Jekyll', 'CSS3'],
+            description: "Elementary is a zero Javascript and minimal CSS ultra lightweight Jekyll theme for those of you who love simplicity.",
+            categories: ['webdev']
+        },
+        {
+            image: 'assets/images/soot-spirits.png',
+            link: 'https://github.com/abhn/Soot-Spirits',
+            title: 'Soot Spirits',
+            demo: 'https://sootspirits.github.io',
+            technologies: ['Jekyll', 'CSS3'],
+            description: "A simple responsive two column Jekyll theme. Great for personal blog and basic portfolio website.",
+            categories: ['webdev']
+        },
+        {
+            image: 'assets/images/python-chat.png',
+            link: 'https://www.nagekar.com/2014/12/lan-group-messenger-in-python.html',
+            title: 'Terminal Group Chat',
+            demo: false,
+            technologies: ['Python', 'Sockets'],
+            description: "Simple terminal group chat based on native sockets using Python.",
+            categories: ['native']
+        },
+        {
+            image: 'assets/images/old-lcd.jpg',
+            link: 'https://www.nagekar.com/2018/05/reusing-old-laptop-lcd-panel.html',
+            title: 'Reusing Old LCD Panel',
+            demo: false,
+            technologies: ['DIY'],
+            description: "Reusing a dead laptop's LCD panel as a secondary monitor.",
+            categories: ['diy']
+        },
+        {
+            image: 'assets/images/nextcloud-enc.png',
+            link: 'https://www.nagekar.com/2017/08/private-cloud-part-2.html',
+            title: 'Encrypted Self-Hosted Cloud',
+            demo: false,
+            technologies: ['NextCloud', 'GnuPG'],
+            description: "Self hosted encrypted cloud setup with Nextcloud and GnuPG.",
+            categories: ['diy', 'security']
+        },
+        {
+            image: 'assets/images/google-cloud-backup.png',
+            link: 'https://www.nagekar.com/2018/05/encrypted-backup-with-duplicity.html',
+            title: 'Encrypted Backups - Google Cloud',
+            demo: false,
+            technologies: ['NextCloud', 'Duplicity'],
+            description: "Create automated encrypted incremental backups of data. Sync everything securely to Google Cloud.",
+            categories: ['diy', 'security']
+        },
+        {
+            image: 'assets/images/pi-cloud.jpg',
+            link: 'https://www.nagekar.com/2016/01/how-to-private-local-cloud-using-raspberrypi.html',
+            title: 'Local Cloud - Raspberry Pi',
+            demo: false,
+            technologies: ['FTP', 'DIY'],
+            description: "Host a local cloud server with a Raspberry Pi and a spare hard disk. Access data instantaneously on any device on the network.",
+            categories: ['diy']
+        },
+        {
+            image: 'assets/images/koalamate.png',
+            link: 'https://github.com/abhn/koalamate',
+            title: 'Koalamate',
+            demo: false,
+            technologies: ['Electron', 'Javascript'],
+            description: "A cross-platform desktop application that serves as a Wolfram Alpha query place and notes taker.",
+            categories: ['native']
         },
     ]
 
@@ -81,7 +173,6 @@ let project_mapper = project => {
         <div class="wrapper">
                 
             <div class="card radius shadowDepth1">
-
                 ${project.image ? 
                     `<div class="card__image border-tlr-radius">
                         <a href="${project.link}">
@@ -89,7 +180,6 @@ let project_mapper = project => {
                         </a>
                     </div>`           
                 : ''}
-
         
                 <div class="card__content card__padding">
         
@@ -98,14 +188,12 @@ let project_mapper = project => {
         
                         <p class="paragraph-text-normal">${project.description} ${project.demo ? `<a href="${project.demo}">Demo</a>` : ''}</p>
                     </article>
-
                                 
                     <div class="card__meta">
                         ${project.technologies.map(tech =>
                             `<span class="project-technology paragraph-text-normal">${tech}</span>`
                         ).join('')}
                     </div>
-
                 </div>
             </div>
         </div>
